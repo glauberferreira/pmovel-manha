2072709a3989f4cd8b1a7f161f3bc5fb64aadfb5
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-03-18 08:19:09 -0300

    adiciona o componente Gato

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Gato from './Gato';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Prof. Glauber Ferreira</Text>
+      <Gato/>
       <StatusBar style="auto" />
     </View>
   );
